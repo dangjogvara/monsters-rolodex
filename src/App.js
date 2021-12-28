d30efc8526a35e0215f98aa1e11ad224662882ca
Dan Poulsen <dangjogvara@gmail.com>
2021-12-28 15:28:26 +0000

    fetch and display objects

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: uuidv4(),
-        },
-        {
-          name: 'Dracula',
-          id: uuidv4(),
-        },
-        {
-          name: 'Zombie',
-          id: uuidv4(),
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users').then(res =>
+      res.json().then(users => this.setState({ monsters: users }))
+    );
   }
 
   render() {
